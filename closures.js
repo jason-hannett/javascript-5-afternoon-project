f3d24edb68ba911ebb48bf6e8be28ae5847fd2af
jason-hannett <jason.hannett@outlook.com>
2020-02-24 14:30:02 -0700

    first commit

--- closures.js
+++ closures.js
@@ -22,7 +22,7 @@ function outer() {
   Invoke outer saving the return value into another variable called 'inner'.
 */
   
-// Code Here
+let inner = outer();
 
 
 
@@ -51,7 +51,10 @@ function callFriend(name) {
   (HINT: You will need to pass in arguments to both function invocations)
 */
 
-//Code Here
+let callJake = callFriend("Jake");
+callJake(435-555-9248)
+
+
 
 
 
@@ -61,16 +64,21 @@ function callFriend(name) {
   Write a function called makeCounter that makes the following code work properly.
 */
 
-//Code Here
+function makeCounter() {
+  let counter = 0;
+  return function() {
+    return counter += 1;
+  };
+}
 
 
 
-//Uncomment this once you make your function
-//   var count = makeCounter();
-//   count(); // 1
-//   count(); // 2
-//   count(); // 3
-//   count(); // 4
+// Uncomment this once you make your function
+  count = makeCounter();
+  count(); // 1
+  count(); // 2
+  count(); // 3
+  count(); // 4
 
 
 
@@ -86,10 +94,14 @@ function callFriend(name) {
 */
 
 function counterFactory(value) {
-  // Code here.
-
+  
   return {
-
+    inc: function(value){
+      return value += 1;
+    },
+    dec: function(value){
+      return value -= 1;
+    }
   };
 }
 
